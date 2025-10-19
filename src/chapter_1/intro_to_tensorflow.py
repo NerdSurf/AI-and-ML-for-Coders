6cd81712047eb27677c64fb718dee2873d6ceb50
TheTwoJs <thetw.js@gmail.com>
2025-10-19 17:52:48 -0600

    WIP: local progress before rebase

--- src/chapter_1/intro_to_tensorflow.py
+++ src/chapter_1/intro_to_tensorflow.py
@@ -1,8 +1,15 @@
-"""
-chapter_1: Introduction to Tensorflow
 
+## traditional programming logic example ##
 
+streetlamp_status: bool = False
+light: float = 1.50
 
-"""
+def streetlamp_on_off(light_brightness, light_status):
+    if light_brightness > 1.00:
+        light_status = False
+    elif light_brightness < 1.00:
+        light_status = True
+    return light_status
 
-var
+def main():
+    streetlamp_on_off(light, streetlamp_status)
